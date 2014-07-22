2df08a51487a767930a8c95833caed06f511fb61
Philip Bauer <bauer@starzel.de>
2014-07-22 15:35:00 +0200

    add addonlist for public consumption

--- src/paragon/site/browser/views.py
+++ src/paragon/site/browser/views.py
@@ -1,5 +1,7 @@
+# -*- coding: UTF-8 -*-
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
 
 class AddonView(BrowserView):
@@ -8,3 +10,19 @@ class AddonView(BrowserView):
 
     def __call__(self):
         return self.template()
+
+
+class AddonList(BrowserView):
+
+    template = ViewPageTemplateFile('addonlist.pt')
+
+    def items(self):
+        results = []
+        catalog = getToolByName(self.context, 'portal_catalog')
+        brains = catalog.unrestrictedSearchResults(portal_type='addon')
+        for brain in brains:
+            results.append(dict(
+                title = brain.Title,
+                state = brain.review_state
+                ))
+        return results
